4270602b4899398c79c05a86bacb2d95db8d6311
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-11-13 12:42:21 +0000

    [finish]フラッシュ、バリデーション設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'book#index'
-  get 'books' => 'homes#index'
-  post 'books' => 'homes#create'
-  get 'books/:id' => 'homes#show'
-  get 'books/:id/edit' => 'homes#edit', as: 'edit_book'
-  patch 'books/:id' => 'homes#update', as: 'update_book'
-  put 'books/:id' => 'homes#update'
-  delete 'books/:id' => 'homes#destroy', as: 'destroy_book' 
+  resources :books, except: [:new]
 end
