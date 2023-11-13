db3135152772fc5414edb144bbe9d831bb5d2ba8
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-11-13 03:09:46 +0000

    [start]画面遷移するようにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'books' => 'homes#index'
-  post 'books' => 'homes#create'
-  get 'books/:id' => 'homes#show'
-  get 'books/:id/edit' => 'homes#edit', as: 'edit_book'
-  patch 'books/:id' => 'homes#update', as: 'update_book'
-  put 'books/:id' => 'homes#update'
-  delete 'books/:id' => 'homes#destroy', as: 'destroy_book' 
-end
+  root to: 'book#index'
+  resources :book #onlyかexcept使う？
+ end
